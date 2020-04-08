58fb92b722dac40e42ac37e7d193512648d149cd
Antonin Boulnois <antonin.boulnois@efrei.net>
2020-04-08 14:33:55 +0200

    create Xcode project

---
+++ glober/glober/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  glober
+//
+//  Created by Antonin Boulnois on 08/04/2020.
+//  Copyright © 2020 Antonin Boulnois. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
