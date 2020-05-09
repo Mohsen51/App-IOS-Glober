21a167afb1bc7f3a6e741db01ef8112b7463a892
Antonin Boulnois <antonin.boulnois@efrei.net>
2020-05-09 09:34:05 +0200

    add full profile

---
+++ glober/glober/DataStructures/FullProfile.swift
@@ -0,0 +1,40 @@
+//
+//  FullProfile.swift
+//  glober
+//
+//  Created by Antonin Boulnois on 08/05/2020.
+//  Copyright © 2020 Antonin Boulnois. All rights reserved.
+//
+
+import Foundation
+
+struct FullProfil: Codable {
+    
+    var success:Int
+    var data:[DataFullProfil]
+    var language:[Langue]
+    var note:[Note]?
+    
+}
+
+struct DataFullProfil: Codable {
+    var FirstName:String
+    var DateOfBirth:Int
+    var Country:String
+    var Gender:Int
+    var Bar:Int
+    var Blabla:Int
+    var Party:Int
+    var Museum:Int
+    var Sport:Int
+    var About:String?
+    
+}
+
+struct Note:Codable{
+    var Note:String?
+}
+
+struct Langue:Codable {
+    var Language:String
+}
